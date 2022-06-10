9ea3859e00a60bdc73b1bf82bd722aef87abe7db
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-10 22:54:48 +0100

    Added BusStops APIView

--- src/bus_stops/views.py
+++ src/bus_stops/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import BusStops
+from .serializers import BusStopsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class BusStopsAPIView(APIView):
+    serializer_class = BusStopsSerializer
+
+    def get(self):
+        bus_stops = BusStops.objects.all()
+        serializer = BusStopsSerializer(bus_stops, many=True)
+
+        return Response(serializer.data)
