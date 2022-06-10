a5d4817499f2a93ca6c6f01121427f9433431197
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-10 22:40:47 +0100

    Fixed issues with migration.

--- src/bus_stops/models.py
+++ src/bus_stops/models.py
@@ -5,8 +5,8 @@ class BusStops(models.Model):
     class Meta:
         db_table = 'stops'
 
-
     name = models.TextField()
     number = models.IntegerField()
     latitude = models.DecimalField(max_digits=30, decimal_places=30)
     longitude = models.DecimalField(max_digits=30, decimal_places=30)
+
