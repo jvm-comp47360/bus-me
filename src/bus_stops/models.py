8d689af0c4214d61841a3723a83b592af44ee885
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-11 10:54:05 +0100

    Fixed loading script and models file.

--- src/bus_stops/models.py
+++ src/bus_stops/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class BusStops(models.Model):
-    id = models.CharField(primary_key=True, max_length=20)
-    name = models.CharField(max_length=20)
+    class Meta:
+        db_table = "bus_stops"
+
+    id = models.CharField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=50)
     number = models.IntegerField()
-    latitude = models.DecimalField(max_digits=30, decimal_places=30)
-    longitude = models.DecimalField(max_digits=30, decimal_places=30)
+    latitude = models.DecimalField(max_digits=19, decimal_places=10)
+    longitude = models.DecimalField(max_digits=19, decimal_places=10)
 
--- src/scripts/load.py
+++ src/scripts/load.py
@@ -1,7 +1,31 @@
 import csv
 import os
+from typing import List
+
+from bus_stops.models import BusStops
 
 
-def run():
-    bus_stops_file_path = open("../../database/stops.csv")
-    bus_stops_file = csv.reader(bus_stops_file_path)
+def run() -> None:
+    """Reads through the Django database"""
+    with open("../../database/stops.csv", "r", encoding="utf8") as bus_stops_file:
+        bus_stops_file_reader = csv.reader(bus_stops_file)
+        next(bus_stops_file_reader, None) # Skips the header row
+
+        # Deletes all existing objects if you need to reset the database
+        BusStops.objects.all().delete()
+
+        for bus_stop_entry in bus_stops_file_reader:
+            add_bus_stop_to_database(bus_stop_entry)
+
+    bus_stops_file.close()
+
+
+def add_bus_stop_to_database(bus_stop_entry: List[str]) -> None:
+    """Reviews a line from the CSV file and adds it to the Django model and database"""
+    bus_stop_id = bus_stop_entry[0]
+    name = bus_stop_entry[1]
+    number = bus_stop_entry[2]
+    latitude = bus_stop_entry[3]
+    longitude = bus_stop_entry[4]
+
+    BusStops.objects.create(id=bus_stop_id, name=name, number=number, latitude=latitude, longitude=longitude)
