d30bdd0b1b16e6e82ba834f571f047399ab26b28
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-11 17:54:19 +0100

    Added test for sub-header in Control Panel.

--- src/frontend/src/components/ControlPanel/controlpanel.test.js
+++ src/frontend/src/components/ControlPanel/controlpanel.test.js
@@ -5,3 +5,9 @@ test("Header should be rendered on screen", () => {
     render(<ControlPanel/>)
     expect(screen.getByText("Where Do You Want To Go?")).toBeInTheDocument()
 })
+
+test("Sub-header should be rendered on screen", () => {
+    render(<ControlPanel/>)
+    expect(screen.getByText("Select a start station and a destination station, and we will give you a realistic " +
+        "estimate of how long your journey is going to take.")).toBeInTheDocument()
+})
