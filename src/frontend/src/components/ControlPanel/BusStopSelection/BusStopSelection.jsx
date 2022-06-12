fb2fd97bfaeb4f9d173de4dcb45ec93622b50e5f
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-12 10:09:24 +0100

    Disabled button if both dropdowns aren't filled.

--- src/frontend/src/components/ControlPanel/BusStopSelection/BusStopSelection.jsx
+++ src/frontend/src/components/ControlPanel/BusStopSelection/BusStopSelection.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import {Autocomplete, TextField} from "@mui/material";
 
-const BusStopSelection = ({busStops}) => {
+const BusStopSelection = ({busStops, label, setId}) => {
 
+  const onChangeHandler = (option, value) => setId(value.id)
 
-  return <div className={"dropdown-container"}>
+  return <>
       <Autocomplete
         getOptionLabel={(option) => `${option.name}, Stop No.${option.number}`}
         options={busStops}
         sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label={"Start"} />}
+        renderInput={(params) => <TextField {...params} label={label} />}
+        onChange={onChangeHandler}
       />
-      <Autocomplete
-        getOptionLabel={(option) => `${option.name}, Stop No.${option.number}`}
-        options={busStops}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label={"Finish"} />}
-      />
-  </div>
+  </>
 }
 
 export default BusStopSelection
--- src/frontend/src/components/ControlPanel/ControlPanel.jsx
+++ src/frontend/src/components/ControlPanel/ControlPanel.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BusStopSelection from "./BusStopSelection/BusStopSelection";
 import Button from '@mui/material/Button';
+import {Stack} from "@mui/material";
 
 const ControlPanel = ({busStops}) => {
+
+  const [startId, setStartId] = useState("")
+  const [finishId, setFinishId] = useState("")
+
+  const buttonDisableHandler = () => startId === "" || finishId === "";
+
   return <div className={"control-panel"}>
     <h1 className={"splash-header"}>Where Do You Want To Go?</h1>
     <p className={"splash-subheader"}>Select a start station and a destination station, and we will give you a realistic estimate of how
       long your journey is going to take.</p>
-    <BusStopSelection busStops={busStops}/>
-    <Button variant={"contained"}>BusMe!</Button>
 
+    <Stack direction={"row"} spacing={2} justifyContent={"center"}>
+      <BusStopSelection busStops={busStops} label={"Start"} setId={setStartId}/>
+      <BusStopSelection busStops={busStops} label={"Finish"} setId={setFinishId}/>
+    </Stack>
+
+    <Button variant={"contained"} disabled={buttonDisableHandler()}>BusMe!</Button>
   </div>
 }
 
