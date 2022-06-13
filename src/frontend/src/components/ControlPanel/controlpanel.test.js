c6b0b4b019cea1536af0e7bc3f2fd26d7f16ad77
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-13 16:35:12 +0100

    Added mock API data for bus stops.

--- src/frontend/src/components/ControlPanel/controlpanel.test.js
+++ src/frontend/src/components/ControlPanel/controlpanel.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ControlPanel from "./ControlPanel.jsx";
 import {render, screen, within} from "@testing-library/react";
 
+const setup = () => render(<ControlPanel/>)
+
 test("All UI elements should appear on the screen", () => {
-  render(<ControlPanel/>)
+  setup()
   expect(screen.getByText("Where Do You Want To Go?")).toBeInTheDocument();
   expect(screen.getByText("Select a start station and a destination station, and we will give you a realistic " +
     "estimate of how long your journey is going to take.")).toBeInTheDocument();
@@ -13,18 +15,6 @@ test("All UI elements should appear on the screen", () => {
 })
 
 test("Button should be be disabled by default", () => {
-  render(<ControlPanel/>)
+  setup()
   expect(screen.getByTestId("submit-button")).toHaveClass("Mui-disabled");
-})
-
-test("Dropdowns should update state", () => {
-  render(<ControlPanel/>)
-
-  const startDropdown = screen.getByTestId("start-dropdown");
-  const input = within(startDropdown).Qu
-  console.log(input)
-  // const finishDropdown = screen.getByTestId("finish-dropdown");
-  console.log(startDropdown)
-  // const handleChange = jest.spyOn(startDropdown.prototype, "onChange")
-
 })
