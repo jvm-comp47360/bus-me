2df8cb0ac7c570fd3656138a84c39b0dcc1b118d
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-17 18:28:04 +0100

    Split the text for the card on to the right hand side of the card.

--- src/frontend/src/components/WeatherCard/WeatherCard.jsx
+++ src/frontend/src/components/WeatherCard/WeatherCard.jsx
@@ -1,34 +1,46 @@
-import {Card, CardMedia, Stack, Typography} from '@mui/material';
+import {Box, Card, CardContent, CardMedia,
+  Typography} from '@mui/material';
 import PropTypes from 'prop-types';
 
 const WeatherCard = ({weather}) => {
   // Unpacking API call here because we want to make
   // sure all weather related activity occurs in this
   // component.
-  const {icon, date} = weather;
+  const {icon, date, weatherText, temperature} = weather;
 
-  return <Card
-    sx={{backgroundColor: '#757DE8', display: 'inline-block'}}
+  return <Card sx={{backgroundColor: '#000000'}}
   >
-    <Stack
-      display={'flex'}
-      flexDirection={'column'}
-      alignItems={'center'}
-      margin={1}
-      spacing={1}
-    >
+    <Box sx={{display: 'flex', flexDirection: 'row'}}>
       <CardMedia
         component={'img'}
         image={require(`../../assets/weather-icons/${icon}.png`)}
         alt={'current weather'}
-        sx={{width: 80}}
+        sx={{width: 90, padding: 3}}
       />
-      <Typography
-        sx={{padding: 0.5, fontSize: 12, color: '#FFFFFF'}}
-      >
-        {date}
-      </Typography>
-    </Stack>
+      <CardContent>
+        <Typography
+          sx={{padding: 0.5,
+            fontSize: 12,
+            color: '#FFFFFF'}}
+        >
+          {date}
+        </Typography>
+        <Typography
+          sx={{padding: 0.5,
+            fontSize: 12,
+            color: '#FFFFFF'}}
+        >
+          {weatherText}!
+        </Typography>
+        <Typography
+          sx={{padding: 0.5,
+            fontSize: 12,
+            color: '#FFFFFF'}}
+        >
+          {temperature}°C
+        </Typography>
+      </CardContent>
+    </Box>
   </Card>;
 };
 
