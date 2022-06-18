0bed6ac1cc1ab30ca444f7e0195d06e13650b937
Jack Mulligan <mulligja1@gmail.com>
2022-06-18 17:30:46 +0100

    Implemented GoogleMap component to appear on the page

--- src/frontend/src/components/Map/Map.jsx
+++ src/frontend/src/components/Map/Map.jsx
@@ -1,10 +1,18 @@
-import {useLoadScript} from '@react-google-maps/api';
+import {useLoadScript, GoogleMap} from '@react-google-maps/api';
 
 const Map = () => {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
   });
-  return isLoaded ? <div>Loaded!</div> : <div>Script loading...</div>;
+  return !(isLoaded) ?
+    <div>Map loading...</div>:
+    <div className="map">
+      <GoogleMap
+        zoom={15}
+        center={{lat: 53.30947559137038, lng: -6.218868208190408}}
+        mapContainerStyle={{width: '100%', height: '100vh'}}>
+      </GoogleMap>
+    </div>;
 };
 
 export default Map;
