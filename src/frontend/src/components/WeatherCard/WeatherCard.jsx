b6004e2e1a67fdfe26606d1a1ebb613ea7e75087
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-18 10:22:10 +0100

    Added new text to WeatherCard, start work on refactoring the text.

--- src/frontend/src/components/WeatherCard/WeatherCard.jsx
+++ src/frontend/src/components/WeatherCard/WeatherCard.jsx
@@ -1,6 +1,11 @@
-import {Box, Card, CardContent, CardMedia,
-  Typography} from '@mui/material';
+import {
+  Box, Card, CardContent, CardMedia, Grid,
+  Typography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
+import {FaTemperatureLow} from 'react-icons/fa';
+import {AiFillCalendar, AiFillBook} from 'react-icons/ai';
+
 
 const WeatherCard = ({weather}) => {
   // Unpacking API call here because we want to make
@@ -8,37 +13,61 @@ const WeatherCard = ({weather}) => {
   // component.
   const {icon, date, weatherText, temperature} = weather;
 
-  return <Card sx={{backgroundColor: '#000000'}}
+  return <Card sx={{backgroundColor: '#757DE8', display: 'inline-block'}}
   >
     <Box sx={{display: 'flex', flexDirection: 'row'}}>
       <CardMedia
         component={'img'}
         image={require(`../../assets/weather-icons/${icon}.png`)}
         alt={'current weather'}
-        sx={{width: 90, padding: 3}}
+        width={'52'}
+        sx={{width: 102, padding: 2}}
       />
-      <CardContent>
-        <Typography
-          sx={{padding: 0.5,
-            fontSize: 12,
-            color: '#FFFFFF'}}
-        >
-          {date}
-        </Typography>
-        <Typography
-          sx={{padding: 0.5,
-            fontSize: 12,
-            color: '#FFFFFF'}}
-        >
-          {weatherText}!
-        </Typography>
-        <Typography
-          sx={{padding: 0.5,
-            fontSize: 12,
-            color: '#FFFFFF'}}
-        >
-          {temperature}°C
-        </Typography>
+      <CardContent sx={{pt: 2.7, alignItems: 'center'}}>
+        <Grid container direction={'row'} alignItems={'center'}>
+          <Grid item>
+            <AiFillCalendar color={'white'}/>
+          </Grid>
+          <Grid item>
+            <Typography
+              sx={{padding: 0.5,
+                fontSize: 12,
+                color: '#FFFFFF'}}
+            >
+              {date}
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container direction={'row'} alignItems={'center'}>
+          <Grid item>
+            <AiFillBook color={'white'}/>
+          </Grid>
+          <Grid item>
+            <Typography
+              sx={{padding: 0.5,
+                fontSize: 12,
+                color: '#FFFFFF'}}
+            >
+              {weatherText}
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container direction={'row'} alignItems={'center'}>
+          <Grid item>
+            <FaTemperatureLow color={'white'}/>
+          </Grid>
+          <Grid item>
+            <Typography
+              sx={{padding: 0.5,
+                fontSize: 12,
+                color: '#FFFFFF'}}
+            >
+              {temperature}°C
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
     </Box>
   </Card>;
