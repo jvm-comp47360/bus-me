1fd13112138ce864c530c98709006bbfd94e7efe
Jack Mulligan <mulligja1@gmail.com>
2022-06-19 12:04:19 +0100

    Added options to the map, implemented best practice for center options and general map option by storing these as a memo

--- src/frontend/src/components/Map/Map.jsx
+++ src/frontend/src/components/Map/Map.jsx
@@ -1,15 +1,28 @@
 import {useLoadScript, GoogleMap} from '@react-google-maps/api';
+import {useMemo} from 'react';
 
 const Map = () => {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
   });
+
+  const centerCoords = useMemo(() => (
+    {lat: 53.30947559137038, lng: -6.218868208190408}
+  ), []);
+  const mapOptions = useMemo(() => (
+    {streetViewControl: false,
+      mapTypeControl: false,
+      clickableIcons: false,
+      fullscreenControl: false}
+  ), []);
+
   return !(isLoaded) ?
     <div className="loading">Map loading...</div>:
     <div className="map">
       <GoogleMap
         zoom={15}
-        center={{lat: 53.30947559137038, lng: -6.218868208190408}}
+        center={centerCoords}
+        options={mapOptions}
         mapContainerStyle={{width: '100%', height: '100vh'}}>
       </GoogleMap>
     </div>;
