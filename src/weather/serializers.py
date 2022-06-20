f1fbd8737a7329645a3d20f210babfe0464cdb2b
HUMPHERY1997 <646401509@qq.com>
2022-06-20 15:55:19 +0100

    Add icon id

--- src/weather/serializers.py
+++ src/weather/serializers.py
@@ -1,7 +1,10 @@
 from dataclasses import field
+from this import s
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 
 
 class WeatherSerializer(serializers.Serializer):  # noqa
+    icon = serializers.CharField(max_length=10)
     weather = serializers.CharField(max_length=200)
     temperature = serializers.DecimalField(max_digits=19, decimal_places=10)
--- src/weather/utils.py
+++ src/weather/utils.py
@@ -18,9 +18,10 @@ def get_current_weather() -> Dict[str, any]:
     lon = -6.248868208190408
     weather_response = requests.get(OpenWeatherAPI_URL.format(lat, lon, OpenWeatherAPI_KEY))
     time.sleep(1)
+    icon = weather_response.json()['weather'][0]['icon']
     weather = weather_response.json()['weather'][0]['main']
     temperature = weather_response.json()['main']['temp']
-    current_weather = {"weather": weather, "temperature": temperature}
+    current_weather = {"icon": icon, "weather": weather, "temperature": temperature}
 
     return current_weather
 
