5dd36facadf2327099cb8d51c329bce8562190b2
Jack Mulligan <mulligja1@gmail.com>
2022-06-20 10:01:39 +0100

    Changed zoom and coord params, set a minZoom on the map, switched divs to containers

--- src/frontend/src/components/Map/Map.jsx
+++ src/frontend/src/components/Map/Map.jsx
@@ -1,27 +1,29 @@
 import {useLoadScript, GoogleMap} from '@react-google-maps/api';
+import {Container} from '@mui/material';
 
 const Map = () => {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
   });
-  const centerCoords = {lat: 53.30947559137038, lng: -6.218868208190408};
+  const centerCoords = {lat: 53.33947559137039, lng: -6.248868208190408};
   const mapOptions = {
     streetViewControl: false,
     mapTypeControl: false,
     clickableIcons: false,
     fullscreenControl: false,
+    minZoom: 10,
   };
 
   return !(isLoaded) ?
-    <div className="loading">Map loading...</div>:
-    <div className="map">
+    <Container className="loading">Map loading...</Container>:
+    <Container disableGutters="true" className="map">
       <GoogleMap
-        zoom={15}
+        zoom={11.7}
         center={centerCoords}
         options={mapOptions}
         mapContainerStyle={{width: '100%', height: '100vh'}}>
       </GoogleMap>
-    </div>;
+    </Container>;
 };
 
 export default Map;
