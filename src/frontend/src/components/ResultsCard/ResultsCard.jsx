2ded56f46d7ff90b9d9ca13cb8ead65dfaf37a76
Jack Mulligan <mulligja1@gmail.com>
2022-06-20 10:51:31 +0100

    Changed how props being destructured, changed card width, cleaned up sx declarations

--- src/frontend/src/components/ResultsCard/ResultsCard.jsx
+++ src/frontend/src/components/ResultsCard/ResultsCard.jsx
@@ -5,21 +5,18 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 
-const ResultsCard = (props) => {
-  const {duration} = props;
+const ResultsCard = ({duration}) => {
   const durationPhrase = duration > 1 ?
   `${duration} Minutes` :
   `${duration} Minute`;
+
   return <Card sx={{
     color: 'white',
     backgroundColor: '#757DE8',
     minWidth: '300px',
-    width: '50vw',
+    width: '33vw',
   }}>
-    <CardContent sx={{
-      textAlign: 'center',
-      pb: 0,
-    }}>
+    <CardContent sx={{textAlign: 'center', pb: 0}}>
       <Typography variant="h6">
         Your journey will take approx.
       </Typography>
@@ -27,10 +24,7 @@ const ResultsCard = (props) => {
         {durationPhrase}
       </Typography>
     </CardContent>
-    <CardActions sx={{
-      justifyContent: 'center',
-      py: 1,
-    }}>
+    <CardActions sx={{justifyContent: 'center', py: 1}}>
       <Button variant='contained'
         sx={{backgroundColor: '#ffeb3b', color: 'black'}}>Close</Button>
     </CardActions>
