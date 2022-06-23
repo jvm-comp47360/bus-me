433cb328690cf20e8dcb8bea7cf696362a082a7b
Jack Mulligan <mulligja1@gmail.com>
2022-06-23 19:19:37 +0100

    Changed App and associated tests to TypeScript formatting

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import '../../styles/main.css';
 import Navbar from '../Navbar/Navbar';
-import ControlPanel from '../ControlPanel/ControlPanel.tsx';
+import ControlPanel from '../ControlPanel/ControlPanel';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import ResultsCard from '../ResultsCard/ResultsCard';
 import Map from '../Map/Map';
@@ -8,7 +8,7 @@ import Map from '../Map/Map';
 import busStopsApi from '../../mockdata/MOCK_BUS_STOPS.json';
 import weatherApi from '../../mockdata/MOCK_WEATHER.json';
 
-const App = () => {
+const App = (): JSX.Element => {
   return <>
     <Navbar />
     <ControlPanel busStops={busStopsApi}/>
