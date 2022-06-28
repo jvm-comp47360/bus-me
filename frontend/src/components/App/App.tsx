93e2a396a69c80293df452f10b7e5012fc672303
toadkarter <vrakhmanin1@gmail.com>
2022-06-28 13:32:13 +0100

    Added initial theme configuration.

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -6,20 +6,24 @@ import ResultsCard from '../ResultsCard/ResultsCard';
 import Map from '../Map/Map';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import {LocalizationProvider} from '@mui/x-date-pickers';
+import {ThemeProvider} from '@mui/material/styles';
 
 import busStopsApi from '../../mockdata/MOCK_BUS_STOPS.json';
 import weatherApi from '../../mockdata/MOCK_WEATHER.json';
+import theme from './BusMeTheme';
 
 const App = (): JSX.Element => {
-  return <LocalizationProvider dateAdapter={AdapterDateFns}>
-    <Navbar />
-    <ControlPanel busStops={busStopsApi}/>
-    <div>
-      <WeatherCard weather={weatherApi[0]}/>
-      <ResultsCard duration={35}/>
-      <Map />
-    </div>
-  </LocalizationProvider>;
+  return <ThemeProvider theme={theme}>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Navbar />
+      <ControlPanel busStops={busStopsApi}/>
+      <div>
+        <WeatherCard weather={weatherApi[0]}/>
+        <ResultsCard duration={35}/>
+        <Map />
+      </div>
+    </LocalizationProvider>
+  </ThemeProvider>;
 };
 
 export default App;
---
+++ frontend/src/components/App/BusMeTheme.tsx
@@ -0,0 +1,14 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#002984',
+    },
+    secondary: {
+      main: '#3f51b5',
+    },
+  },
+});
+
+export default theme;
