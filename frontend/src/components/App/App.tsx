0f31a774acdb7b349f9a0b63bc66d33548d82345
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-29 22:35:18 +0100

    Added API calls to App component.

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,7 +2,7 @@
 import '../../styles/main.css';
 
 // React
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 // Components
 import Navbar from '../Navbar/Navbar';
@@ -26,6 +26,10 @@ import BusStop from '../../types/BusStop';
 import Weather from '../../types/Weather';
 
 const App = (): JSX.Element => {
+  // API urls
+  const busRoutesUrl = 'http://localhost/api/bus_routes';
+  const weatherUrl = 'http://localhost/api/current_weather';
+
   // Information from API calls
   const [busRoutes, setBusRoutes] = useState<BusRoute[]>(busRoutesApi);
   const [weather, setWeather] = useState<Weather>(weatherApi[0]);
@@ -39,6 +43,56 @@ const App = (): JSX.Element => {
   const [routeSelection, setRouteSelection] = useState<BusRoute | null>(null);
   const [dateTimeSelection, setDateTimeSelection] =
       useState<Date | null>(new Date());
+
+  useEffect(() => {
+    // Needs to be declared inside useEffect to be triggered on page load.
+    const getBusRoutes = async (): Promise<BusRoute[]> => {
+      const dataInLocalStorage: string | null =
+          localStorage.getItem('bus_routes');
+      if (dataInLocalStorage) {
+        return getLocalBusRoutes(dataInLocalStorage);
+      } else {
+        return await getBusRoutesFromApi();
+      }
+    };
+
+    const getWeather = async (): Promise<Weather> => {
+      const dataInLocalStorage: string | null =
+          localStorage.getItem('weather');
+      if (dataInLocalStorage) {
+        return getLocalWeather(dataInLocalStorage);
+      } else {
+        return await getWeatherFromApi();
+      }
+    };
+
+    getBusRoutes().then((data: BusRoute[]) => setBusRoutes(data));
+    getWeather().then((data: Weather) => setWeather(data));
+  }, []);
+
+  // Retrieves bus stop data from local storage in JSON format.
+  const getLocalBusRoutes = (dataInLocalStorage: string): BusRoute[] =>
+    JSON.parse(dataInLocalStorage);
+
+  // Retrieves weather data from local storage in JSON format.
+  const getLocalWeather = (dataInLocalStorage: string): Weather =>
+    JSON.parse(dataInLocalStorage);
+
+  // Retrieves bus stop data from API.
+  const getBusRoutesFromApi = async (): Promise<BusRoute[]> => {
+    const busStopsData: BusRoute[] = await fetch(busRoutesUrl)
+        .then((response) => response.json());
+    localStorage.setItem('bus_stops', JSON.stringify(busStopsData));
+    return busStopsData;
+  };
+
+  // Retrieves weather from API.
+  const getWeatherFromApi = async (): Promise<Weather> => {
+    const weatherData: Weather = await fetch(weatherUrl)
+        .then((response) => response.json());
+    localStorage.setItem('weather', JSON.stringify(weatherData));
+    return weatherData;
+  };
 
   return <LocalizationProvider dateAdapter={AdapterDateFns}>
     <Navbar />
