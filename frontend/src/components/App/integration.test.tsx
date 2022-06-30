608a8ec7fb41ca8630a746a00894be340897725f
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-06-30 11:15:39 +0100

    Refactored bus route and stop dropdown tests.

--- frontend/src/components/App/integration.test.tsx
+++ frontend/src/components/App/integration.test.tsx
@@ -9,6 +9,8 @@ import {UserEvent} from '@testing-library/user-event/dist/types/setup';
 import BusStop from '../../types/BusStop';
 
 const BUS_ROUTE_SEARCH: string = BUS_ROUTES_API[0]['name'];
+const BUS_ROUTE_GUESS: string = BUS_ROUTES_API[0]['name'].substring(0);
+const WRONG_BUS_ROUTE_SEARCH: string = BUS_ROUTES_API[1]['name'];
 const BUS_STOPS: BusStop[] = BUS_ROUTES_API[0]['bus_stops'];
 const BUS_STOP_SEARCH =
     `${BUS_STOPS[0]['name']}, Stop No.${BUS_STOPS[0]['number']}`;
@@ -17,6 +19,26 @@ const BUS_STOP_GUESS: string = BUS_STOP_SEARCH.substring(0, 3);
 // Setup function that renders the main component.
 const setup = (): RenderResult => render(<App/>);
 
+describe('<BusRouteDropdown> Functionality of MUI Autocomplete', () => {
+  it('should display the correct route upon user selection',
+      async (): Promise<void> => {
+        expect.assertions(1);
+        setup();
+
+        const busRouteDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /select route/i});
+
+        const view: UserEvent = userEvent.setup();
+
+        await view.click(busRouteDropdown);
+        await view.keyboard(BUS_ROUTE_GUESS);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        expect(busRouteDropdown.value).toBe(BUS_ROUTE_SEARCH);
+      });
+});
+
 describe('<BusStopDropdown> Functionality of MUI Autocomplete', () => {
   it('should not display options if route is not selected',
       async (): Promise<void> => {
@@ -24,7 +46,7 @@ describe('<BusStopDropdown> Functionality of MUI Autocomplete', () => {
         setup();
 
         const busStopDropdown: HTMLInputElement =
-            screen.getByRole('combobox', {name: /start/i});
+                screen.getByRole('combobox', {name: /start/i});
 
         const view: UserEvent = userEvent.setup();
 
@@ -35,16 +57,17 @@ describe('<BusStopDropdown> Functionality of MUI Autocomplete', () => {
 
         expect(busStopDropdown.value).not.toBe(BUS_STOP_SEARCH);
       });
+
   it('should display options if route is selected',
       async (): Promise<void> => {
         expect.assertions(1);
         setup();
 
         const busRouteDropdown: HTMLInputElement =
-            screen.getByRole('combobox', {name: /select route/i});
+                screen.getByRole('combobox', {name: /select route/i});
 
         const busStopDropdown: HTMLInputElement =
-            screen.getByRole('combobox', {name: /start/i});
+                screen.getByRole('combobox', {name: /start/i});
 
         const view: UserEvent = userEvent.setup();
 
@@ -59,5 +82,76 @@ describe('<BusStopDropdown> Functionality of MUI Autocomplete', () => {
         await view.keyboard('[Enter]');
 
         expect(busStopDropdown.value).toBe(BUS_STOP_SEARCH);
+      });
+
+  it('should not display options unrelated to the selected route',
+      async (): Promise<void> => {
+        expect.assertions(1);
+        setup();
+
+        const busRouteDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /select route/i});
+
+        const busStopDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /start/i});
+
+        const view: UserEvent = userEvent.setup();
+
+        await view.click(busRouteDropdown);
+        await view.keyboard(WRONG_BUS_ROUTE_SEARCH);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        await view.click(busStopDropdown);
+        await view.keyboard(BUS_STOP_GUESS);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        expect(busStopDropdown.value).not.toBe(BUS_STOP_SEARCH);
+      });
+});
+
+describe('<ControlPanel> Functionality of SubmitButton', () => {
+  it('should be be disabled by default', () => {
+    expect.assertions(1);
+    setup();
+
+    expect(screen.getByRole('button', {name: /busme!/i}))
+        .toHaveClass('Mui-disabled');
+  });
+
+  it('should be enabled when dropdowns are filled in',
+      async (): Promise<void> => {
+        expect.assertions(1);
+        setup();
+
+        const busRouteDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /select route/i});
+
+        const busStartDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /start/i});
+
+        const busFinishDropdown: HTMLInputElement =
+                screen.getByRole('combobox', {name: /finish/i});
+
+        const view: UserEvent = userEvent.setup();
+
+        await view.click(busRouteDropdown);
+        await view.keyboard(BUS_ROUTE_SEARCH);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        await view.click(busStartDropdown);
+        await view.keyboard(BUS_STOP_GUESS);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        await view.click(busFinishDropdown);
+        await view.keyboard(BUS_STOP_GUESS);
+        await view.keyboard('[ArrowDown]');
+        await view.keyboard('[Enter]');
+
+        expect(screen.getByRole('button', {name: /busme!/i}))
+            .not.toHaveClass('Mui-disabled');
       });
 });
--- frontend/src/components/ControlPanel/BusRouteDropdown/busroutedropdown.test.tsx
+++ frontend/src/components/ControlPanel/BusRouteDropdown/busroutedropdown.test.tsx
@@ -1,50 +1,29 @@
-// import {render, RenderResult, screen} from '@testing-library/react';
-// import BusRouteDropdown from './BusRouteDropdown';
-// import userEvent from '@testing-library/user-event';
-// import {UserEvent} from '@testing-library/user-event/dist/types/setup';
-// import setBusRoutes from '../../App/App';
-// import setBusStops from '../../App/App';
-// import setRouteSelection from '../../App/App';
-// import MOCK_BUS_ROUTES from '../../../mockdata/MOCK_BUS_ROUTES.json';
-//
-// const BUS_ROUTE_SEARCH: string = MOCK_BUS_ROUTES[0]['name'];
-// const LABEL = 'Select Route';
-//
-// const setup = (): RenderResult => render(
-//     <BusRouteDropdown
-//       busRoutes={MOCK_BUS_ROUTES}
-//       setBusRoutes={setBusRoutes}
-//       busStops={[]}
-//       setBusStops={setBusStops}
-//       routeSelection={undefined}
-//       setRouteSelection={setRouteSelection}
-//     />,
-// );
-//
-// describe('<BusRouteDropdown> Functionality of MUI Autocomplete', () => {
-//   it('should show the default text', () => {
-//     expect.assertions(1);
-//     setup();
-//
-//     expect(screen.getByLabelText(LABEL)).toBeInTheDocument();
-//   });
-//
-//   it('should display valid option after text input',
-//       async (): Promise<void> => {
-//         expect.assertions(1);
-//         setup();
-//
-//         const busRouteDropdown: HTMLInputElement =
-//             screen.getByRole('combobox', {name: /select route/i});
-//
-//         const view: UserEvent = userEvent.setup();
-//
-//         await view.click(busRouteDropdown);
-//         await view.keyboard(BUS_ROUTE_SEARCH);
-//         await view.keyboard('[ArrowDown]');
-//         await view.keyboard('[Enter]');
-//
-//         expect(busRouteDropdown.value).toBe(BUS_ROUTE_SEARCH);
-//       });
-// });
+import {render, RenderResult, screen} from '@testing-library/react';
+import BusRouteDropdown from './BusRouteDropdown';
+import setBusRoutes from '../../App/App';
+import setBusStops from '../../App/App';
+import setRouteSelection from '../../App/App';
+import MOCK_BUS_ROUTES from '../../../mockdata/MOCK_BUS_ROUTES.json';
+
+const LABEL = 'Select Route';
+
+const setup = (): RenderResult => render(
+    <BusRouteDropdown
+      busRoutes={MOCK_BUS_ROUTES}
+      setBusRoutes={setBusRoutes}
+      busStops={[]}
+      setBusStops={setBusStops}
+      routeSelection={undefined}
+      setRouteSelection={setRouteSelection}
+    />,
+);
+
+describe('<BusRouteDropdown> Default render', () => {
+  it('should show the default text', () => {
+    expect.assertions(1);
+    setup();
+
+    expect(screen.getByLabelText(LABEL)).toBeInTheDocument();
+  });
+});
 
