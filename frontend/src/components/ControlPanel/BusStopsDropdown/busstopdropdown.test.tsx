516aaa35989a81793324f47917c59034474f8289
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-01 21:42:55 +0100

    Tweaking some of the tests.

--- frontend/src/components/ControlPanel/BusStopsDropdown/busstopdropdown.test.tsx
+++ frontend/src/components/ControlPanel/BusStopsDropdown/busstopdropdown.test.tsx
@@ -1,25 +1,72 @@
-// import {render, RenderResult, screen} from '@testing-library/react';
-// import BusStopDropdown from './BusStopDropdown';
-// import setBusStops from '../../App/App';
-// import setSelection from '../../App/App';
-//
-// const LABEL = 'Start';
-//
-// const setup = (): RenderResult => render(
-//     <BusStopDropdown
-//       busStops={[]}
-//       setBusStops={setBusStops}
-//       label={LABEL}
-//       selection={undefined}
-//       setSelection={setSelection}
-//     />,
-// );
-//
-// describe('<BusStopDropdown> Default render', () => {
-//   it('should show the default text', () => {
-//     expect.assertions(1);
-//     setup();
-//
-//     expect(screen.getByLabelText(LABEL)).toBeInTheDocument();
-//   });
-// });
+import {render, RenderResult, screen} from '@testing-library/react';
+import BusStopDropdown from './BusStopDropdown';
+import setSelection from '../../App/App';
+import BusRoute from '../../../types/BusRoute';
+import MOCK_BUS_ROUTES from '../../../mockdata/MOCK_BUS_ROUTES.json';
+import BusStop from '../../../types/BusStop';
+import {UserEvent} from '@testing-library/user-event/dist/types/setup';
+import userEvent from '@testing-library/user-event';
+
+const LABEL = 'Start';
+const MOCK_ROUTE_SELECTION: BusRoute = MOCK_BUS_ROUTES[0];
+const WRONG_MOCK_ROUTE_SEARCH: BusRoute = MOCK_BUS_ROUTES[1];
+const BUS_STOPS: BusStop[] = MOCK_BUS_ROUTES[0]['bus_stops'];
+const BUS_STOP_SEARCH =
+  `${BUS_STOPS[0]['name']}, Stop No.${BUS_STOPS[0]['number']}`;
+const BUS_STOP_GUESS: string = BUS_STOP_SEARCH.substring(0, 3);
+
+const setup = (routeSelection: BusRoute | undefined):
+  RenderResult => render(
+    <BusStopDropdown
+      busRoutes={MOCK_BUS_ROUTES}
+      routeSelection={routeSelection}
+      label={LABEL}
+      setSelection={setSelection}
+    />,
+);
+
+const mockUserSearch = async (dropdown: HTMLInputElement): Promise<void> => {
+  const view: UserEvent = userEvent.setup();
+
+  await view.click(dropdown);
+  await view.keyboard(BUS_STOP_GUESS);
+  await view.keyboard('[ArrowDown]');
+  await view.keyboard('[Enter]');
+};
+
+describe('<BusStopDropdown> Default render', () => {
+  it('should show the default text', () => {
+    expect.assertions(1);
+    setup(MOCK_ROUTE_SELECTION);
+
+    expect(screen.getByLabelText(LABEL)).toBeInTheDocument();
+  });
+});
+
+describe('<BusStopDropdown> Route selection', () => {
+  it('should display options if route is selected',
+      async (): Promise<void> => {
+        expect.assertions(1);
+        setup(MOCK_ROUTE_SELECTION);
+
+        const busStopDropdown: HTMLInputElement =
+        screen.getByRole('combobox', {name: /start/i});
+
+        await mockUserSearch(busStopDropdown);
+
+        expect(busStopDropdown.value).toBe(BUS_STOP_SEARCH);
+      });
+
+  it('should not display options unrelated to the selected route',
+      async (): Promise<void> => {
+        expect.assertions(1);
+        setup(WRONG_MOCK_ROUTE_SEARCH);
+
+        const busStopDropdown: HTMLInputElement =
+          screen.getByRole('combobox', {name: /start/i});
+
+        await mockUserSearch(busStopDropdown);
+
+        expect(busStopDropdown.value).not.toBe(BUS_STOP_SEARCH);
+      });
+});
--- frontend/src/components/ControlPanel/controlpanel.test.tsx
+++ frontend/src/components/ControlPanel/controlpanel.test.tsx
@@ -1,61 +1,79 @@
-// import {render, RenderResult, screen} from '@testing-library/react';
-//
-// import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
-// import {LocalizationProvider} from '@mui/x-date-pickers';
-//
-// import ControlPanel from './ControlPanel';
-// import setBusRoutes from '../App/App';
-// import setBusStops from '../App/App';
-// import setStartSelection from '../App/App';
-// import setFinishSelection from '../App/App';
-// import setRouteSelection from '../App/App';
-// import setDateTimeSelection from '../App/App';
-// import setPrediction from '../App/App';
-//
-// import MOCK_BUS_STOPS from '../../mockdata/MOCK_BUS_ROUTES.json';
-//
-// const setup = (): RenderResult => render(
-//     <LocalizationProvider dateAdapter={AdapterDateFns}>
-//       <ControlPanel
-//         busRoutes={MOCK_BUS_STOPS}
-//         setBusRoutes={setBusRoutes}
-//         busStops={[]}
-//         setBusStops={setBusStops}
-//         startSelection={undefined}
-//         setStartSelection={setStartSelection}
-//         finishSelection={undefined}
-//         setFinishSelection={setFinishSelection}
-//         routeSelection={undefined}
-//         setRouteSelection={setRouteSelection}
-//         dateTimeSelection={undefined}
-//         setDateTimeSelection={setDateTimeSelection}
-//         prediction={undefined}
-//         setPrediction={setPrediction}
-//       />,
-//     </LocalizationProvider>,
-// );
-//
-// describe('<ControlPanel/> Default rendering', () => {
-//   it('Station dropdowns should appear on the screen', () => {
-//     expect.assertions(2);
-//     setup();
-//
-//     expect(screen.getByRole('combobox', {name: /start/i})).toBeInTheDocument();
-//     expect(screen.getByRole('combobox', {name: /finish/i})).toBeInTheDocument();
-//   });
-//
-//   it('Datetime dropdown should appear on the screen', () => {
-//     expect.assertions(1);
-//     setup();
-//
-//     expect(screen.getByRole('textbox', {name: /choose date/i}))
-//         .toBeInTheDocument();
-//   });
-//
-//   it('submit button should appear on the screen', () => {
-//     expect.assertions(1);
-//     setup();
-//
-//     expect(screen.getByRole('button', {name: /busme!/i})).toBeInTheDocument();
-//   });
-// });
+import {render, RenderResult, screen} from '@testing-library/react';
+
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {LocalizationProvider} from '@mui/x-date-pickers';
+
+import ControlPanel from './ControlPanel';
+import setStartSelection from '../App/App';
+import setFinishSelection from '../App/App';
+import setRouteSelection from '../App/App';
+import setPrediction from '../App/App';
+
+import MOCK_BUS_ROUTES from '../../mockdata/MOCK_BUS_ROUTES.json';
+
+import BusStop from '../../types/BusStop';
+import BusRoute from '../../types/BusRoute';
+
+const MOCK_CURRENT_ROUTE: BusRoute = MOCK_BUS_ROUTES[0];
+const MOCK_START_STATION: BusStop = MOCK_CURRENT_ROUTE['bus_stops'][0];
+const MOCK_FINISH_STATION: BusStop = MOCK_CURRENT_ROUTE['bus_stops'][1];
+
+const setup = (startSelection: BusStop | undefined,
+    finishSelection: BusStop | undefined,
+    routeSelection: BusRoute | undefined,
+): RenderResult => render(
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ControlPanel
+        startSelection={startSelection}
+        setStartSelection={setStartSelection}
+        finishSelection={finishSelection}
+        setFinishSelection={setFinishSelection}
+        routeSelection={routeSelection}
+        setRouteSelection={setRouteSelection}
+        setPrediction={setPrediction}
+      />,
+    </LocalizationProvider>,
+);
+
+describe('<ControlPanel/> Default rendering', () => {
+  it('Station dropdowns should appear on the screen', () => {
+    expect.assertions(2);
+    setup(MOCK_START_STATION, MOCK_FINISH_STATION, MOCK_CURRENT_ROUTE);
+
+    expect(screen.getByRole('combobox', {name: /start/i})).toBeInTheDocument();
+    expect(screen.getByRole('combobox', {name: /finish/i})).toBeInTheDocument();
+  });
+
+  it('Datetime dropdown should appear on the screen', () => {
+    expect.assertions(1);
+    setup(MOCK_START_STATION, MOCK_FINISH_STATION, MOCK_CURRENT_ROUTE);
+
+    expect(screen.getByRole('textbox', {name: /choose date/i}))
+        .toBeInTheDocument();
+  });
+
+  it('submit button should appear on the screen', () => {
+    expect.assertions(1);
+    setup(MOCK_START_STATION, MOCK_FINISH_STATION, MOCK_CURRENT_ROUTE);
+
+    expect(screen.getByRole('button', {name: /busme!/i})).toBeInTheDocument();
+  });
+});
+
+describe('<ControlPanel/> Submit button functionality', () => {
+  it('should be be disabled by default', () => {
+    expect.assertions(1);
+    setup(undefined, undefined, undefined);
+
+    expect(screen.getByRole('button', {name: /busme!/i}))
+        .toHaveClass('Mui-disabled');
+  });
+
+  it('should be be enabled when dropdowns are filled in', () => {
+    expect.assertions(1);
+    setup(MOCK_START_STATION, MOCK_FINISH_STATION, MOCK_CURRENT_ROUTE);
+
+    expect(screen.getByRole('button', {name: /busme!/i}))
+        .not.toHaveClass('Mui-disabled');
+  });
+});
