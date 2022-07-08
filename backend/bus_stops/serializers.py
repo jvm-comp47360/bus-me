891bde7e41ccb4cf8029b9637fb46d5cd8630376
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-08 17:31:04 +0100

    Many-to-many field works for routes.

--- backend/bus_stops/serializers.py
+++ backend/bus_stops/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import BusStops
+from .models import BusStops, BusRoutes, RouteConnections
+
+
+class RouteConnectionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RouteConnections
+        fields = ('bus_route_id', 'bus_stop_id')
+        depth = 2
 
 
 class BusStopsSerializer(serializers.ModelSerializer):
+    bus_routes = BusRoutesSerializer(many=True)
+
     class Meta:
         model = BusStops
         fields = ('id', 'name', 'number', 'latitude', 'longitude')
+        depth = 2
+
+
+class BusRoutesSerializer(serializers.ModelSerializer):
+    bus_stops = BusStopsSerializer(many=True)
+
+    class Meta:
+        model = BusRoutes
+        fields = ('id', 'name', 'bus_stops')
+        depth = 2
+
--- backend/bus_stops/views.py
+++ backend/bus_stops/views.py
@@ -1,14 +1,14 @@
-from .models import BusStops
-from .serializers import BusStopsSerializer
+from .models import BusRoutes
+from .serializers import BusRoutesSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 class BusStopsAPIView(APIView):
-    serializer_class = BusStopsSerializer
+    serializer_class = BusRoutesSerializer
 
     def get(self, request):
-        bus_stops = BusStops.objects.all()
-        serializer = BusStopsSerializer(bus_stops, many=True)
+        bus_routes = BusRoutes.objects.all()
+        serializer = BusRoutesSerializer(bus_routes, many=True)
 
         return Response(serializer.data)
