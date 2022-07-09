e1ca893220d36d50bcf645c03a54f0404ccfa638
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-09 11:47:59 +0100

    Added separate API calls for bus stops and bus routes.

--- backend/bus_stops/urls.py
+++ backend/bus_stops/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('bus_routes/', views.BusRoutesAPIView.as_view()),
     path('bus_stops/', views.BusStopsAPIView.as_view()),
 ]
--- backend/bus_stops/views.py
+++ backend/bus_stops/views.py
@@ -1,14 +1,26 @@
 from .models import BusRoutes
 from .serializers import BusRoutesSerializer
+from .models import BusStops
+from .serializers import BusStopsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class BusStopsAPIView(APIView):
+class BusRoutesAPIView(APIView):
     serializer_class = BusRoutesSerializer
 
     def get(self, request):
         bus_routes = BusRoutes.objects.all()
         serializer = BusRoutesSerializer(bus_routes, many=True)
+
+        return Response(serializer.data)
+
+
+class BusStopsAPIView(APIView):
+    serializer_class = BusStopsSerializer
+
+    def get(self, request):
+        bus_stops = BusStops.objects.all()
+        serializer = BusStopsSerializer(bus_stops, many=True)
 
         return Response(serializer.data)
