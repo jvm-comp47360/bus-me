0a16d262f886c17bde48c3bdf6fa15be695a7dfa
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-10 09:43:24 +0100

    Added relative paths and changed mock bus routes.

--- backend/prediction/utils.py
+++ backend/prediction/utils.py
@@ -4,15 +4,20 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
+from django.conf import settings
 
-LINE_TIME_STOPS = pickle.load(open('Models/LINE_TIME_STOPS.pickle', 'rb'))
+time_stops_url = os.path.join(settings.BASE_DIR, 'prediction/Models/LINE_TIME_STOPS.pickle')
+pickle_url = os.path.join(settings.BASE_DIR, 'prediction/models/LINE_Model/')
+
+LINE_TIME_STOPS = pickle.load(open(time_stops_url, 'rb'))
+
 
 class Prediction:
     def __init__(self, prediction):
         self.prediction = prediction
 
 '''Add Line_Base Prediction Model'''
-def get_prediction(start_stop: str, finish_stop: str, time: str) -> Dict[str, any]:
+def get_prediction(route: str, start_stop: str, finish_stop: str, time: str) -> Dict[str, any]:
     API_KEY = os.environ.get("GOOGLE_DIRECTION_API_KEY")
     url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=transit&transit_mode=bus&key=%s"%(start_stop,finish_stop,API_KEY)
     payload = {}
@@ -20,10 +25,10 @@ def get_prediction(start_stop: str, finish_stop: str, time: str) -> Dict[str, an
     response = requests.request("GET", url, headers=headers, data=payload)
 
     data = response.json()['routes'][0]['legs'][0]['steps']
-    LINEID_MODEL = ['1', '11', '116', '118', '120', '122', '123', '13', 
+    LINEID_MODEL = ['1', '11', '116', '118', '120', '122', '123', '13',
                     '130', '14', '140', '142', '145', '15', '150', '151',
-                    '16', '26', '27', '33', '37', '38', '39', '4', '40', 
-                    '41', '42', '43', '44', '47', '49', '53', '61', '65', 
+                    '16', '26', '27', '33', '37', '38', '39', '4', '40',
+                    '41', '42', '43', '44', '47', '49', '53', '61', '65',
                     '68', '69', '7', '70', '79', '83', '84', '9']
     PREDICT_TIME = 0
     for i in range(len(data)):
@@ -50,11 +55,14 @@ def get_prediction(start_stop: str, finish_stop: str, time: str) -> Dict[str, an
         #     get the PLANNED_DURATION according to index
                 PLANNED_DURATION = df.iloc[PLANNED_DURATION_INDEX]['PLANNED_DURATION']
                 NUM_STOPS = df.iloc[PLANNED_DURATION_INDEX]['NUM_STOPS']
-                pickle_name = 'Models/LINE_Model/LINE_' + LINEID + '.sav'
-                model = pickle.load(open(pickle_name, 'rb'))
+                pickle_name = '/LINE_' + LINEID + '.sav'
+                current_pickle_url = f"{pickle_url}{pickle_name}"
+                model = pickle.load(open(current_pickle_url, 'rb'))
                 PREDICT_TIME += float(model.predict(pd.DataFrame([PLANNED_DURATION]))[0]) * int(NUM_STOP_SEGMENT) / int(NUM_STOPS)
             else:
                 PREDICT_TIME += data[i]['duration']['value']
         else:
             PREDICT_TIME += data[i]['duration']['value']
-    return {"prediction": PREDICT_TIME}
+    output = round(PREDICT_TIME / 60, 2)
+
+    return {"prediction": output}
--- backend/prediction/views.py
+++ backend/prediction/views.py
@@ -9,11 +9,12 @@ class PredictionAPIView(APIView):
     serializer_class = PredictionSerializer
 
     def post(self, request):
+        route = request.data.get('route')
         start_coords = request.data.get('start_coords')
         finish_coords = request.data.get('finish_coords')
         time = request.data.get('time')
 
-        prediction = get_prediction(start_coords, finish_coords, time)
+        prediction = get_prediction(route, start_coords, finish_coords, time)
         serializer = PredictionSerializer(data=prediction)
 
         serializer.is_valid(True)
