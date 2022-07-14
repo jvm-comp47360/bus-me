722dd6562d6aa348f166e41331a591a717db182d
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-14 19:09:33 +0100

    added updated shell script for quicker docker restarting

--- backend/prediction/utils.py
+++ backend/prediction/utils.py
@@ -5,14 +5,14 @@ import pickle
 import os
 from django.conf import settings
 
-time_stops_file_path = os.path.join(settings.BASE_DIR, 'prediction/Models/LINE_TIME_STOPS.pickle')
-pickle_directory_file_path = os.path.join(settings.BASE_DIR, 'prediction/models/LINE_Model/')
-
-line_time_stops_pickle = pickle.load(open(time_stops_file_path, 'rb'))
-
 
 def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, float]:
     """Return dictionary containing prediction given the input variables"""
+
+    time_stops_file_path = os.path.join(settings.BASE_DIR, 'prediction/Models/LINE_TIME_STOPS.pickle')
+    pickle_directory_file_path = os.path.join(settings.BASE_DIR, 'prediction/Models/LINE_Model/')
+
+    line_time_stops_pickle = pickle.load(open(time_stops_file_path, 'rb'))
 
     print(time_stops_file_path)
     print(pickle_directory_file_path)
@@ -31,18 +31,18 @@ def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, f
     planned_duration = df.iloc[planned_duration_index]['PLANNED_DURATION']
     num_stops = int(df.iloc[planned_duration_index]['NUM_STOPS'])
 
-    return {"prediction": get_prediction_from_pickle_file(num_stops, int(num_stops_segment), planned_duration, route)}
+    return {"prediction": get_prediction_from_pickle_file(num_stops, int(num_stops_segment), planned_duration, route, pickle_directory_file_path)}
 
 
-def get_prediction_from_pickle_file(num_stops: int, num_stops_segment: int, planned_duration: int, route: str) -> float:
+def get_prediction_from_pickle_file(num_stops: int, num_stops_segment: int, planned_duration: int, route: str, pickle_directory_file_path: str) -> float:
     """Generate rounded prediction value from provided variables"""
-    model = pickle.load(open(get_current_pickle_file_path(route), 'rb'))
+    model = pickle.load(open(get_current_pickle_file_path(route, pickle_directory_file_path), 'rb'))
     full_route_prediction = float(model.predict(pd.DataFrame([planned_duration]))[0])
     current_segment_prediction = full_route_prediction * num_stops_segment / num_stops
     return round(current_segment_prediction / 60, 2)
 
 
-def get_current_pickle_file_path(route: str) -> str:
+def get_current_pickle_file_path(route: str, pickle_directory_file_path: str) -> str:
     """Generate file path for pickle file that corresponds to currently selected route"""
     pickle_name = '/LINE_' + route + '.sav'
     current_pickle_file_path = f"{pickle_directory_file_path}{pickle_name}"
--- restart_server.sh
+++ restart_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo docker-compose down -v
-sudo docker system prune -a --volumes
+
+sudo docker stop "$(sudo docker ps -a -q)"
+sudo docker rm "$(sudo docker ps -a -q)"
+sudo docker rmi "$(sudo docker images -a -q)"
+
 sudo docker-compose up --build
