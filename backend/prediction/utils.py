3f46331da814f7077145f597136c8ad5aa43a432
HUMPHERY1997 <646401509@qq.com>
2022-07-29 17:30:37 +0100

    Update models and prediction utils

--- backend/prediction/utils.py
+++ backend/prediction/utils.py
@@ -9,40 +9,70 @@ def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, f
     """Return dictionary containing prediction given the input variables"""
     current_file_directory = os.path.dirname(os.path.realpath(__file__))
 
-    print(f"The route is {route}")
-    print(f"The segments are {num_stops_segment}")
-    print(f"The time is {time}")
+    # print(f"The route is {route}")
+    # print(f"The segments are {num_stops_segment}")
+    # print(f"The time is {time}")
 
-    time_stops_file_path = os.path.join(current_file_directory, 'Models/LINE_TIME_STOPS.pickle')
-    pickle_directory_file_path = os.path.join(current_file_directory, 'Models/LINE_Model')
+    search_table_file_path = os.path.join(current_file_directory, 'search_file/search_table.pkl')
+    model_classifer_file_path = os.path.join(current_file_directory, 'search_file/model_classifier.pkl')
+    pickle_directory_file_path = os.path.join(current_file_directory, 'Models')
 
-    line_time_stops_pickle = pickle.load(open(time_stops_file_path, 'rb'))
+    search_table_pickle = pickle.load(open(search_table_file_path, 'rb'))
+    model_classifier_pickle = pickle.load(open(model_classifer_file_path, 'rb'))
 
     # Search pickle file to get the line relate data
-    df = line_time_stops_pickle[line_time_stops_pickle['LINEID'] == str(route)]
+    df = search_table_pickle[search_table_pickle['LINEID'] == str(route)]
+    df = df.reset_index(drop=True)
 
     # Search the most close PLANNED_DURATION index
     planned_duration_index = np.searchsorted(df['PLANNEDTIME_ARR'], time, side='right')
 
-    print(planned_duration_index)
+    # print(planned_duration_index)
 
     # Get the PLANNED_DURATION according to index
     planned_duration = df.iloc[planned_duration_index]['PLANNED_DURATION']
-    num_stops = int(df.iloc[planned_duration_index]['NUM_STOPS'])
+    num_stops = int(df.iloc[planned_duration_index]['NUMOFSTOP'])
 
-    return {"prediction": get_prediction_from_pickle_file(num_stops, int(num_stops_segment), planned_duration, route, pickle_directory_file_path)}
+    # Classifer by lineid and choose what features that should input
+    model_name = model_classifier_pickle[str(route)]
+    if model_name == 'LR':
+        model_feature = ['PLANNED_DURATION']
+        predict_input = [planned_duration]
+    elif model_name == 'HOUR_LR' or 'HOUR_RF':
+        model_feature = ['PLANNED_DURATION', 'HOUR_sin', 'HOUR_cos']
+        hour_sin,hour_cos = time_encoder(time)
+        predict_input = [planned_duration,hour_sin,hour_cos]
+    else:
+        model_feature = ['PLANNED_DURATION', 'HOUR_sin', 'HOUR_cos', 'DISTANCE']
+        hour_sin,hour_cos = time_encoder(time)
+        distance = df.iloc[planned_duration_index]['DISTANCE']
+        predict_input = [planned_duration,hour_sin,hour_cos,distance]
+
+    return {"prediction": get_prediction_from_pickle_file(num_stops, int(num_stops_segment), model_feature, predict_input, route, pickle_directory_file_path)}
 
 
-def get_prediction_from_pickle_file(num_stops: int, num_stops_segment: int, planned_duration: int, route: str, pickle_directory_file_path: str) -> float:
+def get_prediction_from_pickle_file(num_stops: int, num_stops_segment: int, model_feature: list, predict_input: list, route: str, pickle_directory_file_path: str) -> float:
     """Generate rounded prediction value from provided variables"""
     model = pickle.load(open(get_current_pickle_file_path(route, pickle_directory_file_path), 'rb'))
-    full_route_prediction = float(model.predict(pd.DataFrame([planned_duration]))[0])
+    if len(model_feature)>1:
+        full_route_prediction = float(model.predict(pd.DataFrame([predict_input],columns=model_feature)))
+    else:
+        full_route_prediction = float(model.predict(pd.DataFrame([predict_input[0]])))
     current_segment_prediction = full_route_prediction * num_stops_segment / num_stops
     return round(current_segment_prediction / 60, 2)
 
 
 def get_current_pickle_file_path(route: str, pickle_directory_file_path: str) -> str:
     """Generate file path for pickle file that corresponds to currently selected route"""
-    pickle_name = '/LINE_' + route + '.sav'
+    pickle_name = '/LINE_' + route + '.pkl'
     current_pickle_file_path = f"{pickle_directory_file_path}{pickle_name}"
     return current_pickle_file_path
+
+def time_encoder(time: int) -> float:
+    if time>=86400:
+        hour = int((time-86400)/3600)
+    else:
+        hour = int(time/3600)
+    hour_sin = np.sin(2 * np.pi * hour/23)
+    hour_cos = np.cos(2 * np.pi * hour/23)
+    return hour_sin,hour_cos
