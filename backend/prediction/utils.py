df54e13ab9d8d94c8c41519ac7aeb7dec124ae5e
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-07-31 14:57:21 +0100

    Tweaks to type hints.

--- backend/prediction/utils.py
+++ backend/prediction/utils.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 import pandas as pd
 import numpy as np
 import pickle
@@ -8,10 +8,6 @@ import os
 def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, float]:
     """Return dictionary containing prediction given the input variables"""
     current_file_directory = os.path.dirname(os.path.realpath(__file__))
-
-    # print(f"The route is {route}")
-    # print(f"The segments are {num_stops_segment}")
-    # print(f"The time is {time}")
 
     search_table_file_path = os.path.join(current_file_directory, 'search_file/search_table.pkl')
     model_classifer_file_path = os.path.join(current_file_directory, 'search_file/model_classifier.pkl')
@@ -27,8 +23,6 @@ def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, f
     # Search the most close PLANNED_DURATION index
     planned_duration_index = np.searchsorted(df['PLANNEDTIME_ARR'], time, side='right')
 
-    # print(planned_duration_index)
-
     # Get the PLANNED_DURATION according to index
     planned_duration = df.iloc[planned_duration_index]['PLANNED_DURATION']
     num_stops = int(df.iloc[planned_duration_index]['NUMOFSTOP'])
@@ -40,22 +34,32 @@ def get_prediction(route: str, num_stops_segment: str, time: str) -> Dict[str, f
         predict_input = [planned_duration]
     elif model_name == 'HOUR_LR' or 'HOUR_RF':
         model_feature = ['PLANNED_DURATION', 'HOUR_sin', 'HOUR_cos']
-        hour_sin,hour_cos = time_encoder(time)
-        predict_input = [planned_duration,hour_sin,hour_cos]
+        hour_sin,hour_cos = time_encoder(int(time))
+        predict_input = [planned_duration, hour_sin, hour_cos]
     else:
         model_feature = ['PLANNED_DURATION', 'HOUR_sin', 'HOUR_cos', 'DISTANCE']
-        hour_sin,hour_cos = time_encoder(time)
+        hour_sin, hour_cos = time_encoder(int(time))
         distance = df.iloc[planned_duration_index]['DISTANCE']
         predict_input = [planned_duration,hour_sin,hour_cos,distance]
 
-    return {"prediction": get_prediction_from_pickle_file(num_stops, int(num_stops_segment), model_feature, predict_input, route, pickle_directory_file_path)}
+    return {"prediction": get_prediction_from_pickle_file(num_stops,
+                                                          int(num_stops_segment),
+                                                          model_feature,
+                                                          predict_input,
+                                                          route,
+                                                          pickle_directory_file_path)}
 
 
-def get_prediction_from_pickle_file(num_stops: int, num_stops_segment: int, model_feature: list, predict_input: list, route: str, pickle_directory_file_path: str) -> float:
+def get_prediction_from_pickle_file(num_stops: int,
+                                    num_stops_segment: int,
+                                    model_feature: list,
+                                    predict_input: list,
+                                    route: str,
+                                    pickle_directory_file_path: str) -> float:
     """Generate rounded prediction value from provided variables"""
     model = pickle.load(open(get_current_pickle_file_path(route, pickle_directory_file_path), 'rb'))
-    if len(model_feature)>1:
-        full_route_prediction = float(model.predict(pd.DataFrame([predict_input],columns=model_feature)))
+    if len(model_feature) > 1:
+        full_route_prediction = float(model.predict(pd.DataFrame([predict_input], columns=model_feature)))
     else:
         full_route_prediction = float(model.predict(pd.DataFrame([predict_input[0]])))
     current_segment_prediction = full_route_prediction * num_stops_segment / num_stops
@@ -68,11 +72,12 @@ def get_current_pickle_file_path(route: str, pickle_directory_file_path: str) ->
     current_pickle_file_path = f"{pickle_directory_file_path}{pickle_name}"
     return current_pickle_file_path
 
-def time_encoder(time: int) -> float:
-    if time>=86400:
+
+def time_encoder(time: int) -> Tuple[float, float]:
+    if time >= 86400:
         hour = int((time-86400)/3600)
     else:
         hour = int(time/3600)
     hour_sin = np.sin(2 * np.pi * hour/23)
     hour_cos = np.cos(2 * np.pi * hour/23)
-    return hour_sin,hour_cos
+    return hour_sin, hour_cos
