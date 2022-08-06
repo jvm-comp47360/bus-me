31ed784df74f06723a8802af3f1906001d209542
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-08-06 16:57:52 +0100

    Removed unnecessary pickles.

--- frontend/src/components/DisplayPanel/JourneyPanel/JourneyLeg/JourneyLeg.tsx
+++ frontend/src/components/DisplayPanel/JourneyPanel/JourneyLeg/JourneyLeg.tsx
@@ -30,18 +30,6 @@ const JourneyLeg = ({
     routeSelection,
     prediction, directions, predictionStages}: Props): JSX.Element => {
 
-    const stationPickles = ['68', '14', '77A', '39', '16', '40D',
-        '27B', '142', '83', '130', '15', '46A', '33', '7', '39A',
-        '1', '41', '59', '9', '40', '84', '53', '185', '151', '13',
-        '15B', '65B', '61', '140', '123', '79A', '38A', '69', '44',
-        '42', '184', '238', '145', '27A', '17', '27X', '122', '54A',
-        '150', '56A', '37', '27', '15A', '65', '47', '76', '79', '83A',
-        '63', '4', '120', '41C', '70', '84A', '220', '32X', '68A', '84X',
-        '38', '102', '270', '33X', '75', '26', '49', '111', '18', '11',
-        '114', '44B', '7A', '43', '104', '33D', '39X', '41B', '7D', '46E',
-        '118', '51D', '15D', '38D', '40B', '38B', '236', '7B', '41X',
-        '40E', '161', '69X', '116', '77X', '16D', '33E', '41D'];
-
     const getArrivalTime = (depatureTime: Date, prediction: number): Date => {
         const startUnixTime: number = depatureTime.getTime();
         const predictionInMillisecs: number = prediction * 60 * 1000;
